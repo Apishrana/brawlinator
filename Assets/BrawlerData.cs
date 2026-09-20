d534791e8fecb7b5c62858a530bea3751a30628c
Apishrana <apishrana682@gmail.com>
2026-09-20 13:46:56 +0530

    data structs for brawler

--- Assets/BrawlerData.cs
+++ Assets/BrawlerData.cs
@@ -5,10 +5,26 @@ using UnityEngine;
 public class BrawlerData : ScriptableObject
 {
     public string Name;
-    public Sprite icon;
+    public Sprite Icon;
 
-    public genderEnum gender;
-    // public
+    public genderEnum Gender;
+    public rarityEnum Rarity;
+    public classEnum Class;
+    public speedEnum Speed;
+
+    public bool Robot;
+    public bool Jump;
+    public bool DestroyWalls;
+    public bool DestroyBush;
+    public bool Stun;
+    public bool Slow;
+    public bool Family;
+    public bool BypassWall;
+    public bool Splash;
+    public bool Burn;
+    public bool Poison;
+    public bool Invisibility;
+    public bool Buffies;
 }
 
 public enum genderEnum
@@ -17,5 +33,13 @@ public enum genderEnum
 }
 public enum rarityEnum
 {
-    male, female
+    Starting, Rare, Super_Rare, Epic, Mythic, Legendary, Ultra
+}
+public enum classEnum
+{
+    Tank, Assassin, Support, Controller, Damage_Dealer, Marksman, Artillery
+}
+public enum speedEnum
+{
+    Normal, Fast, Very_Fast, Very_Slow, Slow
 }
