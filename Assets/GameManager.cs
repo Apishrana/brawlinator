b1834430152ef0aa44974660b80e38f93c9c092b
Apishrana <apishrana682@gmail.com>
2026-09-20 22:58:46 +0530

    question data

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,16 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private List<BrawlerData> brawlers;
+    [SerializeField]
+    private List<QuestionData> questions;
+    private List<BrawlerData> candidates;
+
+    private int questionIndex;
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
